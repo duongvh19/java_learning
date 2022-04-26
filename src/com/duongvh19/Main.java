886ef62c574a153a16badbd2cd95cd6a5081308e
duongvh19 <duongvh1921@gmail.com>
2022-04-26 21:33:25 +0700

    [Java] Thread and Runnable

--- src/com/duongvh19/Main.java
+++ src/com/duongvh19/Main.java
@@ -1,8 +1,34 @@
 package com.duongvh19;
 
+import static com.duongvh19.ThreadColor.ANSI_GREEN;
+import static com.duongvh19.ThreadColor.ANSI_PURPLE;
+import static com.duongvh19.ThreadColor.ANSI_RED;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println(ANSI_PURPLE+"Hello from the main thread.");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
+        anotherThread.start();
+
+        new Thread() {
+            public void run() {
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
+            }
+        }.start();
+
+
+        Thread myRunnableThread = new Thread(new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+            }
+        });
+
+        myRunnableThread.start();
+
+        System.out.println(ANSI_PURPLE+"Hello again from the main thread.");
     }
 }
---
+++ src/com/duongvh19/MyRunnable.java
@@ -0,0 +1,11 @@
+package com.duongvh19;
+
+import static com.duongvh19.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run()");
+    }
+}
