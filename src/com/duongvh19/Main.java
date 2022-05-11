013bf3dcefc5d05557c09997e73dff3c977a6b48
duongvh19 <duongvh1921@gmail.com>
2022-05-11 21:28:56 +0700

    [java] thread-deadlocks

--- src/com/duongvh19/Main.java
+++ src/com/duongvh19/Main.java
@@ -2,7 +2,49 @@ package com.duongvh19;
 
 public class Main {
 
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+
+        new Thread1().start();
+        new Thread2().start();
+    }
+
+    private static class Thread1 extends Thread {
+
+        public void run() {
+            synchronized (lock1){
+                System.out.println("Thread1: Has lock1.");
+                try{
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+
+                }
+                System.out.println("Thread1: Waiting for lock 2.");
+                synchronized (lock2){
+                    System.out.println("Thread1: Has lock1 and lock2");
+                }
+            }
+            System.out.println("Thread1: Released lock1. Exiting...");
+        }
+    }
+
+    private static class Thread2 extends Thread {
+        public void run() {
+            synchronized (lock2){
+                System.out.println("Thread1: Has lock2.");
+                try{
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+
+                }
+                System.out.println("Thread1: Waiting for lock 2.");
+                synchronized (lock1){
+                    System.out.println("Thread1: Has lock2 and lock2");
+                }
+            }
+            System.out.println("Thread2: Released lock2. Exiting...");
+        }
     }
 }
