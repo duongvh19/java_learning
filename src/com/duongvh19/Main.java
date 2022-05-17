583ea5c09113eda180c37be1d0c5bcdc1d763b13
duongvh19 <duongvh1921@gmail.com>
2022-05-17 21:23:06 +0700

    [java] lambda-lesson1

--- src/com/duongvh19/Main.java
+++ src/com/duongvh19/Main.java
@@ -1,8 +1,87 @@
 package com.duongvh19;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        /*
+
+        new Thread(new CodeToRun()).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printing from the Runnable");
+            }
+        }).start();
+
+        // using lambda
+        new Thread(()-> System.out.println("Printing from the Runnable")).start();
+
+        new Thread(()-> {
+            System.out.println("Printing from the Runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n", 3);
+        }).start();
+
+         */
+
+        Employee tom = new Employee("Tom", 21);
+        Employee tim = new Employee("Tim", 22);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 27);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(tom);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        });
+
+        for (Employee e : employees) {
+            System.out.println(e.getName());
+        }
+    }
+}
+class CodeToRun implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Printing from the Runnable");
+    }
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
