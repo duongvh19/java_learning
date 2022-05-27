816f90f2eeebbec200b18eab3b378bc6dcdecc16
duongvh19 <duongvh1921@gmail.com>
2022-05-27 23:20:49 +0700

    [java] streams

--- src/com/duongvh19/Main.java
+++ src/com/duongvh19/Main.java
@@ -1,8 +1,35 @@
 package com.duongvh19;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        List<String> someBingoNumbers = Arrays.asList(
+                "N40", "N38", "N27",
+                "A26", "A45",
+                "B15", "B17", "B45",
+                "G54", "G48", "G67", "G37", "G25", "g55"
+        );
+
+        someBingoNumbers
+                .stream()
+                .map(s-> s.toUpperCase())
+                .filter(s->s.startsWith("G"))
+                .sorted()
+                .forEach(s -> System.out.println(s));
+        System.out.println("============");
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I27", "I45", "I60");
+        Stream<String> inNumberStream = Stream.of("N40", "N48", "I26", "I27", "I45", "I60");
+        Stream<String> concatStreams = Stream.concat(ioNumberStream,inNumberStream);
+
+        System.out.println(concatStreams
+                .distinct()
+                .peek(s -> System.out.println(s))
+                .count());
+
     }
 }
