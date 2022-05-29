d9669cc966ee93c56cee7e8423b0af7a02a6d4b4
duongvh19 <duongvh1921@gmail.com>
2022-05-29 21:45:14 +0700

    [java] streams part 2

--- src/com/duongvh19/Main.java
+++ src/com/duongvh19/Main.java
@@ -1,7 +1,9 @@
 package com.duongvh19;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -30,6 +32,38 @@ public class Main {
                 .distinct()
                 .peek(s -> System.out.println(s))
                 .count());
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Haven", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 27);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream()
+                .flatMap(department -> department.getEmployee().stream())
+                .forEach(d -> System.out.println(d));
+
+        //sortedGNumbers
+        System.out.println("-----------------------");
+        List<String> sortedGNumbers = someBingoNumbers.stream()
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(Collectors.toList());
+
+        for (String s : sortedGNumbers) {
+            System.out.println(s);
+        }
 
     }
 }
